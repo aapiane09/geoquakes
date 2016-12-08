66ac2e41a812f96c287e8a28b51bed34c663fdcf
aapiane09 <aapiane09@gmail.com>
2016-12-07 17:29:09 -0800

    Trying to set up maps element

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 // define globals
+
+
+//USGS Earthquake data
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).on("ready", function() {
-
+//Grab USGS data
   $.ajax({
      method: 'GET',
-     url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+     url: weekly_quakes_endpoint,
      data: $('body').serialize(),
      dataType: 'JSON',
      success: onSubmitReqSuccess,
@@ -15,13 +18,17 @@ $(document).on("ready", function() {
 
   function onSubmitReqSuccess (responseData){
     console.log("Success!", responseData)
+    //Set up Handlebars
     var source = $('#info-template').html();
     var template = Handlebars.compile(source);
-    // responseData.features.forEach(function(responseData){
-    //     var earthquakeHtml = template({title : properties.title, time: properties.time});
-    //     var paragraphString = "'<p>{{title}} + ' / ' + {{time}} + ' hours ago</p>'";
-    //     $('#info').append(paragraphString)
-    // })
+    //Convert time data and inject it into HTML document
+    responseData.features.forEach(function(earthQ){
+        var now = Date.now();
+        var eqTime = earthQ.properties.time;
+        var timeEquasion = (((now - eqTime)/86400000)*24);
+        var earthquakeHtml = template({title : earthQ.properties.title, time: Math.round(timeEquasion)});
+        $('#info').append(earthquakeHtml)
+    })
   }
   function onComplete (){
     console.log("Completed!");
@@ -29,6 +36,8 @@ $(document).on("ready", function() {
   function onError (){
     console.log("Error!")
   };
+
+
 });
 
 //API Key for Google Maps = AIzaSyB4vSX9T4soW_LEStFBLKWOpKPQlZbCYmI
